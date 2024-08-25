9bbf18f78f50e0c875d934ee296b08eeb8fdc010
Ramudgar <ramudgary52@gmail.com>
2024-08-25 13:53:49 +0545

    profile create at app.js

--- KharalJiiEcommerce/Backend/app.js
+++ KharalJiiEcommerce/Backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const dotenv=require("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 3000;
 const connectDB = require("./src/config/db");
@@ -9,9 +9,39 @@ app.use(express.json());
 
 connectDB();
 
-const userRoutes=require("./src/routes/userRoutes");
-app.use('/api/user', userRoutes);
- 
+const userRoutes = require("./src/routes/userRoutes");
+app.use("/api/user", userRoutes);
+
+const profileModel = require("./src/models/userProfile");
+app.post("/profile/create", async (req, res) => {
+  const data = req.body;
+  if (
+    !data.firstName ||
+    !data.lastName ||
+    !data.email ||
+    !data.phone ||
+    !data.address
+  ) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  try {
+    const profile = new profileModel({
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      phone: data.phone,
+      address: data.address,
+    });
+
+    const response = await profile.save();
+    return res
+      .status(201)
+      .json({ msg: "Profile created successfully", response });
+  } catch (err) {
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
---
+++ KharalJiiEcommerce/Backend/src/models/userProfile.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userProfileSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const profile=mongoose.model("Profile", userProfileSchema);
+module.exports = profile;
