5884945bc98fdd080a2d3e0b45b2e72a90db73d7
Ramudgar <ramudgary52@gmail.com>
2024-09-25 14:17:00 +0545

    data is fetched successfully

--- KharalJiiEcommerce/Frontend/ecommerce/src/Components/Card/cardComponent.jsx
+++ KharalJiiEcommerce/Frontend/ecommerce/src/Components/Card/cardComponent.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const CardComponent = ({datas}) => {
+const CardComponent = ({ datas }) => {
+  const domain = `http://localhost:8000`;
+  const imgAddress = (item)=>{
+    if(item.productImage){
+      return `${domain}/${item.productImage}`
+    }else{
+      return item.imgUrl;
+    }
+  }
+ 
   return (
-    <div className="flex flex-wrap justify-around items-center my-2 gap-2">
-    {datas.map((item) => {
-      return (
-        <div className="max-w-sm rounded-lg  overflow-hidden shadow-lg ">
-          <img
-            className="w-full h-64 object-cover p-5 bg-cover rounded "
-            src={item.imgUrl}
-            alt={item.alt}
-            
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2"> {item.name}</div>
-            <p className="text-gray-700 text-base">
-             {item.description}
-            </p>
-          </div>
-          <div className="px-6 py-4 text-left">
-            <button className="bg-black text-white px-5 py-2 text-center">
-              Learn more..
-            </button>
-          </div>
-        </div>
-      );
-    })}
-  </div>
-  )
-}
 
-export default CardComponent
+
+
+    <div className="flex flex-wrap justify-around items-center my-2 gap-2">
+      {datas.map((item) => {
+        return (
+          <div className="max-w-sm rounded-lg  overflow-hidden shadow-lg ">
+            <img
+              className="w-full h-64 object-cover p-5 bg-cover rounded "
+              src={`${imgAddress(item)}` }
+              alt={item.alt}
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2"> {item.name}</div>
+              <p className="text-gray-700 text-base">{item.description}</p>
+              <p>{item.price}</p>
+            </div>
+            <div className="px-6 py-4 text-left">
+              <button className="bg-black text-white px-5 py-2 text-center">
+                Learn more..
+              </button>
+            </div>
+          </div>
+        );
+
+        
+      })}
+    </div>
+  );
+  
+};
+
+export default CardComponent;
--- KharalJiiEcommerce/Frontend/ecommerce/src/Components/Product/productComponent.jsx
+++ KharalJiiEcommerce/Frontend/ecommerce/src/Components/Product/productComponent.jsx
@@ -1,50 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardComponent from "../Card/cardComponent";
+import axios from "axios";
+const domain = `http://localhost:8000`;
 
-const ProductComponent = () => {
-  const products = [
-    {
-      imgUrl:
-        "https://fashionmingle.com/wp-content/uploads/2021/07/beauty-4993472_1920-e1635781134472.jpg",
-      name: "Laptop",
-      description: "This laptop is very good",
+const ProductComponent = (id) => {
+  const [products, setProducts] = useState([]);
 
-    },
-    {
-      name: "Mobile",
-      description: "This mobile is very good",
-      imgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4_dQhCOznjcVJVBOncME6jWtEWTTzv7_bA&s",
-    },
-    {
-      name: "Tablet",
-      description: "This tablet is very good",
-      imgUrl:
-        "https://orchidlifesciences.com/wp-content/uploads/2024/06/01-14-01-1024x704.jpg",
-        alt:"fashion beauty"
-    },
-    {
-        name: "iphone",
-        description: "This ipone is best",
-        imgUrl:
-          "https://orchidlifesciences.com/wp-content/uploads/2024/06/01-14-01-1024x704.jpg",
-      },
-      {
-        name: "iphone",
-        description: "This ipone is best",
-        imgUrl:
-          "https://orchidlifesciences.com/wp-content/uploads/2024/06/01-14-01-1024x704.jpg",
-      },
-      {
-        name: "iphone",
-        description: "This ipone is best",
-        imgUrl:
-          "https://orchidlifesciences.com/wp-content/uploads/2024/06/01-14-01-1024x704.jpg",
-      },
-  ];
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(`${domain}/api/products`);
+      console.log(response.data.products);
+      setProducts(response.data.products);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <CardComponent datas={products} />
+      {products.length === 0 ? (
+        <h1 className="text-center">Loading...</h1>
+      ) : (
+        <CardComponent datas={products} />
+      )}
     </>
   );
 };
